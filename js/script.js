4be06f382dfa6f9f7424176500bb4e4514b6effc
Sergio Merida <sergiotabarini@hotmail.com>
2014-11-24 20:35:18 -0600

    VF

--- js/script.js
+++ js/script.js
@@ -6,8 +6,14 @@ image = [];
 OrderList = [];
 OrderDuration = [];
 OrderPlayCount = [];
+image = [];
+urls = [];
 
 
+$(document).ready(function() {
+	$("[data-toggle=popover]")
+	.popover({html:true})
+});
 
 jQuery(document).ready(function($) {
   $.ajax({
@@ -23,12 +29,12 @@ jQuery(document).ready(function($) {
         artist.push(parsed_json['tracks']['track'][i]['artist']['name'])
 	duration.push(parsed_json["tracks"]["track"][i]["duration"])
         playcounts.push(parsed_json["tracks"]["track"][i]["playcount"])
+	urls.push(parsed_json["tracks"]["track"][i]["artist"]["url"])
 
         try {image.push(parsed_json["tracks"]["track"][i]["image"][2]["#text"])}
-        catch(err){image.push("https://www.wordans.ca/wvc-1332825671/wordansfiles/images/2012/3/27/133476/133476_340.jpg")}
+        catch(err){image.push("img/1416895040_photo-128.png")}
 
-        var image2 = "http://img268.imageshack.us/img268/4378/buttonplayc.png"
-        $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+        $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + "<a href=\""+urls[i]+"\">" + artist[i] + "</a>" + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
       };
     }
   });
@@ -73,7 +79,7 @@ $(document).ready(function() {
 
     for (var i = 0; i <50; i++) {
         var play = "http://img268.imageshack.us/img268/4378/buttonplayc.png"
-        $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+        $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
     };
   });
 
@@ -84,7 +90,7 @@ $(document).ready(function() {
     for (var x = 0; x <50; x++) {
       for (var i = 0; i <50; i++) {
         if (OrderList[x] === song[i]) {
-          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
         };
       };
     };
@@ -98,7 +104,7 @@ $(document).ready(function() {
       for (var i = 0; i <50; i++) {
         if (OrderList[x] === song[i]) {
           var play = "http://img268.imageshack.us/img268/4378/buttonplayc.png"
-          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
         };
       };
     };
@@ -110,7 +116,7 @@ $(document).ready(function() {
     for (var x = 0; x <50; x++) {
       for (var i = 0; i <50; i++) {
         if (OrderDuration[x] === duration[i]) {
-          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
         };
       };
     };
@@ -122,7 +128,7 @@ $(document).ready(function() {
     for (var x = 0; x <50; x++) {
       for (var i = 0; i <50; i++) {
         if (OrderPlayCount[x] === playcounts[i]) {
-          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
+          $(".mostrar").append("<tr>" + "<td>" + (i+1) + "</td>" + "<td class='name'>" + song[i] + "</td>" + "<td>" + "<img src=\""+image[i]+"\" class = 'image' height='50px'>" + "</td>" + "<td>" + artist[i] + "</td>" + "<td>" + playcounts[i] + "</td>" + "<td>" + (duration[i]/60).toFixed(2) + "</td>" + "</tr>");
         };
       };
     };
